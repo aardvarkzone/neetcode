a3bc2c05c48a720f4d111d3e41f5c92fd4148938
Advait Arumugam <advaitarumugam@gmail.com>
2024-11-08 14:47:21 -0800

    Update two_integer_sum.py

--- two_integer_sum.py
+++ two_integer_sum.py
@@ -1,11 +1,17 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hashmap = {}
-        for index, value in enumerate(nums):
-            complement = target - value
-            if complement in hashmap:
-                return[hashmap[complement], index]
+        #make a hashmap - key: num, value: index
+        #find comp = target - num 
+        #   - if comp in hash_map, return [hash_map[num], index]
+        #   - else, hash_map[num] = index
+        #no matches = []
+        #hash_map[comp] < index because if its is in hash_map, it must've been a previous index
+        hash_map = {}
+        for index, num in enumerate(nums): 
+            comp = target - num
+            if comp in hash_map:
+                return[hash_map[comp], index]
+            hash_map[num] = index
 
-            hashmap[value] = index
-
+        
         return []
