8a1d9460e501c2b496e757f7bdcc763c096a11ed
Advait Arumugam <advaitarumugam@gmail.com>
2024-11-08 15:11:18 -0800

    Update top_k_elements_in_list.py

--- top_k_elements_in_list.py
+++ top_k_elements_in_list.py
@@ -1,12 +1,28 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        #unique solution
+        #hash: key = num in nums, value = count of key
+        #sort by count (non-ascending), return list of first k elems
+        #use .items() to get (key, value) pairs for sort (item[1] = value)
+
+        #notes: lambda = function collapse
+        #list slicing:
+            # [start:stop] start is inclusive, stop is exclusive
+            # [:] entire list copy (shallow)
+            # [::step] every step element
+            # [::-1] reverse the list
+            # [-n:] last n elements
+            # [n:] from index n to the end
+            # [:n] first n elements excluding index n
+            # [:-n] all elements except the last n
+
         hash_map = {}
-        #key is num, val is freq
+
         for num in nums: 
-            if num not in hash_map: 
-                hash_map[num] = 1
-            else:
+            if num in hash_map: 
                 hash_map[num] += 1
+            else: 
+                hash_map[num] = 1
         
-        sort = sorted(hash_map.items(), key=lambda item: item[1], reverse = True)
-        return [item[0] for item in sort[:k]]
+        sorted_hash = sorted(hash_map.items(), key = lambda item: item[1])
+        return [item[0] for item in sorted_hash[-k:]]
