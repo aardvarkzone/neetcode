b822b532eedc809ce99f1224804eb497833fb4d9
Advait Arumugam <advaitarumugam@gmail.com>
2024-11-08 14:54:21 -0800

    Update anagram_groups.py

--- anagram_groups.py
+++ anagram_groups.py
@@ -1,13 +1,17 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        #hash_map with linked list 
+        #key is sorted word, list holds anagrams 
+        #to sort, use ''.join(sorted())
+        #return list(hash_map.values())
         
         hash_map = {}
-        for string in strs:
-            sort = ''.join(sorted(string))
-            if sort not in hash_map:
-                hash_map[sort] = []
-            
-            hash_map[sort].append(string)
-            
 
+        for string in strs: 
+            sorted_str = ''.join(sorted(string))
+            if sorted_str not in hash_map:
+                hash_map[sorted_str] = [] 
+            
+            hash_map[sorted_str].append(string)
+        
         return list(hash_map.values())
