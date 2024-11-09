cd7aeb1b52b4afcb4582142c587eb18fff17c70a
Advait Arumugam <advaitarumugam@gmail.com>
2024-11-08 16:37:43 -0800

    Update longest_consecutive_sequence.py

--- longest_consecutive_sequence.py
+++ longest_consecutive_sequence.py
@@ -1,18 +1,26 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        if not nums: return 0
+        #if empty, return 0
+        #sort nums
+        #if nums[i] == nums[i-1] + 1, add to streak
+        #if nums[i] == nums[i-1], keep streak same and continue
+        #else add streak to list, reset streak 
+        #return max of list 
+        if not nums: return 0 
         nums.sort()
+
+        counter_list = []
+        streak = 1
+
+        for i in range(1, len(nums)): 
+            if nums[i] == nums[i - 1] + 1: 
+                streak += 1
+            elif nums[i] == nums[i - 1]: 
+                continue 
+            else: 
+                counter_list.append(streak)
+                streak = 1
+        #handles case of longest string being last one
+        counter_list.append(streak)
         
-        counter = []
-        current_streak = 1  
-        
-        for i in range(1, len(nums)):
-            if nums[i] == nums[i - 1] + 1: current_streak += 1   
-            elif nums[i] == nums[i - 1]: continue
-            else:
-                counter.append(current_streak)
-                current_streak = 1
-        
-        counter.append(current_streak)
-        
-        return max(counter)
+        return max(counter_list)
