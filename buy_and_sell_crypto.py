8a3f3b967818933b772406712c557ff997657317
Advait Arumugam <advaitarumugam@gmail.com>
2024-11-10 13:44:44 -0800

    Update buy_and_sell_crypto.py

--- buy_and_sell_crypto.py
+++ buy_and_sell_crypto.py
@@ -1,16 +1,18 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        left = 0
+        #two pointer sliding window 
+        #variable window 
+        #first --> check if left < right
+        #if not, set left equal to right, increment right by 1 
+        #if left < right, then check for max output, incr right by 1
+        left = 0 
         right = 1
-        output = 0
+        output = 0 
 
         while right < len(prices): 
             if prices[left] < prices[right]: 
-                profit = prices[right] - prices[left]
-                output = max(output, profit)
+                output = max(prices[right] - prices[left], output)
             else: 
                 left = right
-        
             right += 1
-
         return output
