0eb30cc964150fbe536e8b56cc459772a32d8a4e
Advait Arumugam <advaitarumugam@gmail.com>
2024-11-10 12:58:59 -0800

    Update validate_parentheses.py

--- validate_parentheses.py
+++ validate_parentheses.py
@@ -1,15 +1,23 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        stack = [] 
-        matching_dict = { ")" : "(", "]" : "[", "}" : "{" }
+        #stack method
+        #use a map that has closed as key, opened as value
+        #if char is in map, check if previous thing on stack is the open
+        #if char is in map but prev thing is not, then false
+        #else append to stack 
+
+        stack = []
+        hash_map = {'}' : '{', ')' : '(', ']' : '['}
 
         for char in s: 
-            if char in matching_dict: 
-                if stack and stack[-1] == matching_dict[char]:
+            if char in hash_map:
+                if stack and stack[-1] == hash_map[char]:
                     stack.pop()
                 else: 
                     return False
-            else: 
+            else:
                 stack.append(char)
 
-        return True if not stack else False
+        if stack:
+            return False
+        else: return True
